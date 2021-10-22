ef97a4a58c85672481bd6f6abac3d02ca53fadac
lewfer <34630995+lewfer@users.noreply.github.com>
2021-10-22 08:46:14 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+namespace plotter {
+    export function plotColumn (col: number, height: number) {
+        for (let index = 0; index <= height; index++) {
+            led.plot(0, 0)
+        }
+    }   
+}
