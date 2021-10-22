a6d1fb000b97cc1ae9502bb7443c6c6333ed7aab
lewfer <34630995+lewfer@users.noreply.github.com>
2021-10-22 08:49:07 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 namespace plotter {
+    //% blockId=plotColumn
+    //% block="plot column %height at %col"
     export function plotColumn (col: number, height: number) {
         for (let index = 0; index <= height; index++) {
             led.plot(0, 0)
